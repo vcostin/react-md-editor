8185e8ca0b1b09f92ece4b24aa1b78d7462619ad
Vadim Costin <vadim.costin@yopeso.com>
2017-05-29 19:56:57 +0300

    Add react hotreload boilerplate

---
+++ src/components/PlaceHolder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PlaceHolder extends Component {
+  render() {
+    return (
+      <div className="PlaceHolder"><h1>Placeholder</h1></div>
+    );
+  }
+}
+
+export default PlaceHolder;
