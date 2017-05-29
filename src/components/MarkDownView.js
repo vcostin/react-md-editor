86c28d3025fc40eaeea58023572aa0d2b93732dc
Vadim Costin <vadim.costin@yopeso.com>
2017-05-30 01:33:09 +0300

    Remove sanitize-html

--- src/components/MarkDownView.js
+++ src/components/MarkDownView.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import marked from 'marked';
-import sanitizeHtml from 'sanitize-html';
 
 class MarkDownView extends Component {
   render() {
     return (
       <div
         className="MarkDownView md-element markdown-body"
-        dangerouslySetInnerHTML={{ __html: sanitizeHtml(marked(this.props.mdViewContent)) }}
+        dangerouslySetInnerHTML={{ __html: marked(this.props.mdViewContent) }}
       />
     );
   }
